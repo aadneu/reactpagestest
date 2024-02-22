5545875c923da863927cbecf73f493bbf8d10390
aadneu <aadneu@gmail.com>
2024-02-22 14:10:38 +0100

    2commit

--- pagesappone/src/App.js
+++ pagesappone/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DETTE ER ÅPENBART EN TEST
         </p>
         <a
           className="App-link"
